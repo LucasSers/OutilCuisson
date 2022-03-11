5b62f967239eb304c59398e940451e418baa74f3
Val-Nost <valentin.smn81@gmail.com>
2022-03-11 13:25:28 +0100

    Réglage problème quand liste vide

--- app/src/main/java/com/example/outilcuisson/Afficher.java
+++ app/src/main/java/com/example/outilcuisson/Afficher.java
@@ -146,17 +146,26 @@ public class Afficher extends Fragment {
             return;
         }
 
-        int totalHeight = 0;
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            View listItem = listAdapter.getView(i, null, listView);
-            listItem.measure(0, 0);
-            totalHeight += listItem.getMeasuredHeight();
-        }
+        // si la liste est vide, on règle la hauteur du layout à 0
+        if (listAdapter.isEmpty()) {
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+            params.height = 0;
+            listView.setLayoutParams(params);
+            listView.requestLayout();
+        } else { // sinon on récupère la hauteur nécessaire avec le nombre d'item dans la liste
 
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-        params.height = totalHeight
-                + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
-        listView.setLayoutParams(params);
-        listView.requestLayout();
+            int totalHeight = 0;
+            for (int i = 0; i < listAdapter.getCount(); i++) {
+                View listItem = listAdapter.getView(i, null, listView);
+                listItem.measure(0, 0);
+                totalHeight += listItem.getMeasuredHeight();
+            }
+
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+            params.height = totalHeight
+                    + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+            listView.setLayoutParams(params);
+            listView.requestLayout();
+        }
     }
 }
